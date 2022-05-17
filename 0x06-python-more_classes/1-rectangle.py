991b794efbdf90a92ebbe2054608867016b6f649
premiereelementllc <premiereelementllc@gmail.com>
2022-05-16 21:03:52 -0500

    m

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-"""this is a rectangle class"""
+'''defines a rectangle'''
 
 
 class Rectangle:
-    """Rectangle class
-       Args:
-       width: int
-       height int
-    """
+    '''defines a rectangle'''
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
-    @property
-    def height(self):
-        """int: height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError('height must be an integer')
-        elif value < 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
-        """int: width"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError('width must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height * 2) + (self.__width * 2)
