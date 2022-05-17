bbe065732d17c1eda9e6c82cad3cf3610bb48535
premiereelementllc <premiereelementllc@gmail.com>
2022-05-17 12:07:05 -0500

    m

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-
 import random
 number = random.randint(-10000, 10000)
-
-if (number < 0):
-    number = number * (-1)
-    last_digit = number % 10
-    last_digit = last_digit * (-1)
-    number = number * (-1)
-    message = "and is less than 6 and not 0"
-    print("Last digit of {} is {} {}".format(number, last_digit, message)
+if number < 0:
+    last_digit = ((number * -1) % 10) * -1
 else:
-	last_digit = number % 10
-	if (last_digit == 0):
-        message = "and is 0"
-		print("Last digit of {} is {} {} and is 0".format(number, last_digit, message))
-	elif (last_digit > 5):
-        message = "and is greater that 5"
-		print("Last igit of {} is {} {}".format(number, last_digit, message))
-	else: 
-        message = "and is less than 6 and not 0"
-		print("Last digit of {} is {}".format(number, last_digit, message))
+    last_digit = number % 10
+if last_digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
+elif last_digit > 5:
+    print("Last digit of {} is {}".format(number, last_digit), end=" ")
+    print("and is greater than 5")
+else:
+    print("Last digit of {} is {}".format(number, last_digit), end=" ")
+    print("and is less than 6 and not 0")
