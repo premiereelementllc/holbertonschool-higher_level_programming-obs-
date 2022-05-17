0927f2927a70e7f8205dcc26e9d563b88a97a7b3
premiereelementllc <premiereelementllc@gmail.com>
2022-05-17 14:31:29 -0500

    m

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-'''this is a prototype that returns an integer'''
+"""
+This module contains one function, add_integer()
+"""
 
 
 def add_integer(a, b=98):
-    if isinstance(a, float):
-        a = int
-    elif not isinstance(a, int):
-        raise TypeError("a must be an integer")
-
-    if isinstance(b, float):
-        b = int
-
-    elif not isinstance(a, int):
-        raise TypeError("b must be an integer")
-    '''adding both integers as a return'''
+    """
+    Return the sum of a and b
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
