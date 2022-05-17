465ba36d97eaccef70228028341bcf3cdaa50415
premiereelementllc <premiereelementllc@gmail.com>
2022-05-17 14:07:17 -0500

    m

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python#
-
-def sqaure_matrix_simple(matrix=[]):
-new_matrix = ((matrix=[])^2)
-
-return (new_matrix)
-
-
-
+#!/usr/bin/python3                                                  
+                                                                    
+def square_matrix_simple(matrix=[]):                                
+                                                                    
+        new_matrix = []                                             
+        tmp = []                                                    
+        for row in matrix:                                          
+                for element in row:                                 
+                        tmp.append(element**2)                      
+                new_matrix.append(tmp)                              
+                tmp = []                                            
+                                                                    
+        return (new_matrix)                                         
+                               
--- 0x03-python-more_data_structures/3-common_elements.py
+++ 0x03-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & 
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for elem in reversed(my_list):
+            print("{:d}".format(elem)) 
