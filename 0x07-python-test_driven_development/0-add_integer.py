89f7f16886caa86013399fcbd1cbda4c90defd3a
premiereelementllc <premiereelementllc@gmail.com>
2022-05-17 19:25:20 -0500

    m

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains one function, add_integer()
-"""
+'''this is a notation'''
 
 
 def add_integer(a, b=98):
-    """
-    Return the sum of a and b
-    """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    '''this is a notation'''
+    if type(a) != int:
+        if type(a) == float:
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if type(b) != int:
+        if type(b) == float:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return (a + b)
