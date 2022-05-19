89543cbd0dcbe36eb2ed51d510b25dbe9a5a1807
premiereelementllc <premiereelementllc@gmail.com>
2022-05-18 21:07:10 -0500

    m

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Args:
+        obj: object
+        class: object
+    """
+    return type(obj) is a_class
