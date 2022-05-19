f9ca41f4d3eac5e779f3734d3dd34cb3288c6bad
premiereelementllc <premiereelementllc@gmail.com>
2022-05-18 21:28:55 -0500

    m

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Geometry module
+Classes:
+    BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    this class is a base of Geometry
+    """
+
+    def area(self):
+        """
+        blueprint method
+        """
+        raise Exception("area() is not implemented")
