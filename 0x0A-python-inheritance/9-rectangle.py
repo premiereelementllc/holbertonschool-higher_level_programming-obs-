00216e57c386728c199e2efbda193e214c16bb01
premiereelementllc <premiereelementllc@gmail.com>
2022-05-18 21:39:21 -0500

    m

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this is a notation'''
+
+
+class MyList(list):
+    '''this is a notation'''
+    def print_sorted(self):
+        '''this is a notation'''
+        print(sorted(self))
